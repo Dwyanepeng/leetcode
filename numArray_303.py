811b624aca7d7aa51fe50443de88826369f3be56
dwyanepeng <dwyanepeng@163.com>
2019-08-08 17:59:06 +0800

    数组区间求和

---
+++ numArray_303.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/8/8 17:17
+# @Site    : 
+# @File    : numArray_303.py
+# @Software: PyCharm
+'''给定一个整数数组  nums，求出数组从索引 i 到 j  (i ≤ j) 范围内元素的总和，包含 i,  j 两点。
+
+示例：
+
+给定 nums = [-2, 0, 3, -5, 2, -1]，求和函数为 sumRange()
+
+sumRange(0, 2) -> 1
+sumRange(2, 5) -> -1
+sumRange(0, 5) -> -3
+说明:
+
+你可以假设数组不可变。
+会多次调用 sumRange 方法。
+'''
+class NumArray(object):
+    def __init__(self, nums):
+        self.nums = nums
+    def sumRange(self, i, j):
+        n = len(self.nums)
+        li = list(range(0, n))
+        if i in li and j in li:
+            return self.nums[i]+self.nums[j]
+
+nn = NumArray([-2, 0, 3, -5, 2, -1])
+# nn.__init__()
+print(nn.sumRange(0,2))
+print(nn.sumRange(2,5))
+print(nn.sumRange(0,5))
+
+#数组区间的和
+class NumArray1(object):
+    def __init__(self, nums):
+        self.nums = nums
+        self.sums = []
+        total = 0
+        for n in nums:
+            total += n
+            self.sums.append(total)
+
+    def sumRange(self, i, j):
+        if i > 0:
+            return self.sums[j] - self.sums[i-1]
+        else:
+            return self.sums[j]
